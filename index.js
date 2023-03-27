0db3abf25a4f294ea9e4b2adf8e60d6e07e0de2f
Luke Mendiola <luke.mendi02@gmail.com>
2023-03-26 19:42:09 -0500

    Created popup windows for new lists and new groups

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-function showPopUp(){
+function showPopUp1(){
     document.getElementById("popup-1").classList.toggle("active");
+}
+function showPopUp2(){
+    document.getElementById("popup-2").classList.toggle("active");
 }
