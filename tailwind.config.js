4a7780663d015de307467ec0eb183823c973fbac
Julian Rocha <73362783+Julian-Rocha@users.noreply.github.com>
2023-03-26 19:06:58 -0500

    test tailwind link

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./pages/*.{html,js}"],
+  content: ["./index.html"],
   theme: {
     screens: {
       tablet: "960px",
