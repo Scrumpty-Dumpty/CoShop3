686136df414671df53e98da9b7388ebb050698af
Julian Rocha <73362783+Julian-Rocha@users.noreply.github.com>
2023-03-28 12:20:38 -0500

    removed unecessary messages href

--- components/topnav.js
+++ components/topnav.js
@@ -8,7 +8,7 @@ class Topnav extends HTMLElement {
       <!-- Top Menu -->
       <div class="absolute top-0 right-0 z-10 flex items-center gap-8 p-4">
         <button class="cursor-not-allowed"><i class="fa-solid fa-plus fa-xl"></i></button>
-        <a href="" class="cursor-not-allowed">
+        <a class="cursor-not-allowed">
           <i class="fa-solid fa-message fa-xl"></i>
         </a>
         <img src="./assets/img/man-1.png" alt="" class="h-10" />
