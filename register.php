61c81094686756c7815892ab1e22022281e5dda9
Julian Rocha <73362783+Julian-Rocha@users.noreply.github.com>
2023-04-01 00:49:36 -0500

    updated UI

--- register.php
+++ register.php
@@ -38,20 +38,37 @@ if(isset($_POST['submit'])){
 ?>
 
 <!DOCTYPE html>
-<html lang = "en">
-<head>
-        <meta charset = "UTF-8">
-        <meta http-equiv = "X-UA-Compatible" content = "IE=edge">
-        <meta name = "viewport" content = "width=deveice-width, intitial-scale=1.0">
-        <title>Register</title>
-        <!-- custom css file link -->
-        <link rel = "stylesheet" href = "style.css">
-</head>
-<body>
-    <div class = "form-container">
-        <form action = "" method="post" enctype = "multipart/form-data">
-            <h3>Register Now</h3>
-            <?php
+<html lang="en-us">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>CoShop | Register</title>
+    <link rel="icon" href="./assets/logo.svg" type="image/svg+xml" />
+    <link href="./assets/styles/output.css" rel="stylesheet" />
+  </head>
+  <body>
+    <div class="absolute top-4 left-4">
+      <div class="flex items-center">
+        <img src="./assets/logo.svg" alt="logo" class="h-10" />
+        <p class="text-lg">
+          <span class="font-bold">Co</span><span>Shop</span>
+        </p>
+      </div>
+    </div>
+    <main class="grid min-h-screen grid-cols-2">
+      <div
+        class="col-span-2 flex items-center justify-center text-center lg:col-span-1"
+      >
+        <form
+          class="grid gap-2"
+          action=""
+          method="post"
+          enctype="multipart/form-data"
+        >
+          <h1 class="text-5xl font-black">Create an Account</h1>
+          <p class="text-lg">Get started for free!</p>
+          <?php
             if(isset($message)){
                 foreach($message as $message){
                     echo '<div class = "message">'.$message.'</div>';
@@ -59,16 +76,96 @@ if(isset($_POST['submit'])){
             }
             ?>
 
-            <input type = "name" name = "name" placeholder= "Enter username" class = "box" required>
-            <input type = "email" name = "email" placeholder= "Enter email" class = "box" required>
-            <input type = "password" name = "password" placeholder= "Enter password" class = "box" required>
-            <input type = "password" name = "confpassword" placeholder= "Confirm password" class = "box" required>
-            <input type = "file"  name = "image" class= "box" accept = "image/jpg, image/jpeg, image/png">
-            <input type = "submit" name = "submit" value = "Register" class = "btn">
-            <p>Already have an account? <a href = "login.php">Login here.</a></p>
+          <img
+            id="pic-preview"
+            src="./assets/img/blank-profile.jpg"
+            class="mx-auto aspect-square w-36 rounded-full object-cover"
+          />
+          <input
+            id="profile-picture"
+            type="file"
+            accept="image/jpg, image/jpeg, image/png"
+            onchange="readURL(this);"
+            class="hidden"
+          />
+          <label
+            for="profile-picture"
+            class="flex items-center justify-center gap-2 text-sm"
+          >
+            <i
+              class="fa-solid fa-camera text-lg text-gray-500 hover:text-gray-600"
+            ></i>
+            <span class="text-gray-500 hover:text-gray-600"
+              >Upload a profile picture</span
+            >
+          </label>
 
-</form>
-</div>
-</body>
+          <input
+            class="border-b border-gray-200 py-2 pl-2"
+            type="text"
+            name="name"
+            placeholder="Full Name"
+            autocomplete="name"
+            required
+          />
+
+          <input
+            class="border-b border-gray-200 py-2 pl-2"
+            type="email"
+            name="email"
+            placeholder="Email"
+            autocomplete="email"
+            required
+          />
+
+          <input
+            class="mt-1 border-b border-gray-200 py-2 pl-2"
+            type="password"
+            name="password"
+            placeholder="Password"
+            required
+          />
+
+          <input
+            class="mt-1 border-b border-gray-200 py-2 pl-2"
+            type="password"
+            name="confpassword"
+            placeholder="Confirm password"
+            required
+          />
+          <button
+            type="submit"
+            class="mt-2 bg-yellow-500 py-2 font-bold text-white hover:bg-yellow-600"
+          >
+            Register
+          </button>
+          <a href="" class="text-right text-sm font-bold">Privacy Policy</a>
+          <p class="mt-1">
+            Already a member?
+            <a href="./login.html" class="font-bold">Login here</a>
+          </p>
+        </form>
+      </div>
+
+      <div class="hidden lg:grid">
+        <div
+          class="flex items-center justify-center"
+          data-aos="fade-left"
+          data-aos-duration="700"
+          data-aos-once="true"
+        >
+          <div class="h-5/6 w-5/6 rounded-full bg-yellow-400"></div>
+          <img
+            src="./assets/img/app-window.png"
+            alt=""
+            class="absolute h-2/4 rounded-lg"
+          />
+        </div>
+      </div>
+    </main>
+    <script src="./assets/scripts/scripts.js" defer></script>
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+  </body>
 </html>
+
 
