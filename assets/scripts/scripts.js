d34fd86445e631d30dae06947506d8fc8611ddaf
Julian Rocha <73362783+Julian-Rocha@users.noreply.github.com>
2023-04-03 11:58:03 -0500

    updated function name

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -6,6 +6,21 @@ function readURL(input) {
       document
         .getElementById("pic-preview")
         .setAttribute("src", e.target.result);
+      console.log(e.target.result);
+    };
+
+    reader.readAsDataURL(input.files[0]);
+  }
+}
+
+function readURLPP(input) {
+  if (input.files && input.files[0]) {
+    const reader = new FileReader();
+
+    reader.onload = function (e) {
+      document.getElementById("pic-preview").style.backgroundImage =
+        "url('" + e.target.result + "')";
+      console.log(e.target.result);
     };
 
     reader.readAsDataURL(input.files[0]);
