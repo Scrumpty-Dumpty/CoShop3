0bc4e496259b7f7decc2b36e21bef8ae2beb0763
Julian Rocha <73362783+Julian-Rocha@users.noreply.github.com>
2023-04-09 22:51:56 -0500

    updated UI

--- components/topnav.js
+++ components/topnav.js
@@ -6,12 +6,16 @@ class Topnav extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
       <!-- Top Menu -->
-      <div class="absolute top-0 right-0 z-10 flex items-center gap-8 p-4">
-        <button class="cursor-not-allowed"><i class="fa-solid fa-plus fa-xl"></i></button>
-        <a class="cursor-not-allowed">
-          <i class="fa-solid fa-message fa-xl"></i>
-        </a>
-        <img src="./assets/img/man-1.png" alt="" class="h-10" />
+      <div class="absolute top-0 right-0 z-10 flex items-center gap-7 p-4">
+        <button><i class="fa-solid fa-plus fa-xl"></i></button>
+        <button>        
+          <i class="fa-solid fa-bell fa-xl relative">
+            <div id="statusBubble" class="absolute -top-3 -right-[0.10rem] border bg-red-500 h-[0.75rem] w-[0.75rem] rounded-full"></div>
+          </i>
+        </button>
+        <div class="h-10 w-10 relative bg-cover bg-center" style="background-image: url('./assets/img/man-1.png')">
+          <div id="statusBubble" class="absolute bottom-0 right-0 border bg-green-500 h-3 w-3 rounded-full"></div>
+        </div>
       </div>
     `;
   }
