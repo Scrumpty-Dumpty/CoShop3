94434bc5a80c534825203c90f952d0a6a81f9693
Julian Rocha <73362783+Julian-Rocha@users.noreply.github.com>
2023-04-13 21:44:14 -0500

    7-14 organized codebase

--- src/components/topnav.js
+++ src/components/topnav.js
@@ -7,13 +7,13 @@ class Topnav extends HTMLElement {
     this.innerHTML = `
       <!-- Top Menu -->
       <div class="absolute top-0 right-0 z-10 flex items-center gap-7 p-4">
-        <button><i class="fa-solid fa-plus fa-xl"></i></button>
+        <button onclick="showPopUpAddItem()"><i class="fa-solid fa-plus fa-xl"></i></button>
         <button>        
           <i class="fa-solid fa-bell fa-xl relative">
             <div id="statusBubble" class="absolute -top-3 -right-[0.10rem] border bg-red-500 h-[0.75rem] w-[0.75rem] rounded-full"></div>
           </i>
         </button>
-        <div class="h-10 w-10 relative bg-cover bg-center" style="background-image: url('./assets/img/man-1.png')">
+        <div class="h-10 w-10 relative bg-cover bg-center" style="background-image: url('/src/assets/img/man-1.png')">
           <div id="statusBubble" class="absolute bottom-0 right-0 border bg-green-500 h-3 w-3 rounded-full"></div>
         </div>
       </div>
